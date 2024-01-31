e66f2f39a6bfb432223ddad1f86bf7d2ac90338b
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2024-01-31 22:13:11 +0530

    instagram skeleton done

--- pages/2_😎_Instagram.py
+++ pages/2_😎_Instagram.py
@@ -1,8 +1,16 @@
 import streamlit as sl
 
-sl.set_page_config(
-    page_title="Media Box",
-    page_icon="📱"
-)
+sl.set_page_config(page_title="Media Box", page_icon="📱")
 
 sl.title("Instagram Reels Downloader")
+
+with sl.form("Enter the Youtube video URL", clear_on_submit=False):
+    inp_url = sl.text_input("video URL")
+    submit_btn = sl.form_submit_button("Download")
+
+if submit_btn:
+    try:
+        sl.success("Video Downloaded. See your Downloads folder.")
+    except:
+        sl.error("Enter the correct URL.")
+
