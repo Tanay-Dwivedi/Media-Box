8475cd9bcce9811346307ad06bef8b1ca62abb8f
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2024-01-31 23:45:24 +0530

    homepage update

--- 📝_Homepage.py
+++ 📝_Homepage.py
@@ -1,8 +1,23 @@
 import streamlit as sl
 
-sl.set_page_config(
-    page_title="Media Box",
-    page_icon="📱"
-)
+sl.set_page_config(page_title="Media Box", page_icon="📱")
 
 sl.title("Media Box")
+
+sl.write("##")
+
+sl.write("## Facebook Video Downloader")
+sl.write("_will write how to use_")
+sl.markdown("""-----""")
+
+sl.write("## Instagram Reels Downloader")
+sl.write("_will write how to use_")
+sl.markdown("""-----""")
+
+sl.write("## Youtube Video Downloader")
+sl.write("_will write how to use_")
+sl.markdown("""-----""")
+
+sl.write("## Mp4 to Mp3 converter")
+sl.write("_will write how to use_")
+sl.markdown("""-----""")
