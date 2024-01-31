1d44571546702ddacc3830931ff6532f7fa2f90d
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2024-01-31 23:40:14 +0530

    Convertly skeleton done

--- pages/4_🔉_Converty.py
+++ pages/4_🔉_Converty.py
@@ -6,3 +6,14 @@ sl.set_page_config(
 )
 
 sl.title("Mp4 to Mp3 converter")
+
+with sl.form("Upload the video", clear_on_submit=False):
+    inp_url = sl.file_uploader("Upload file")
+    submit_btn = sl.form_submit_button("Convert to mp3")
+
+if submit_btn:
+    try:
+        sl.success("Audio Downloaded. See your Downloads folder.")
+    except:
+        sl.error("Enter the correct file.")
+
